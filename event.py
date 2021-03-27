c3f2158acd78550df385a3092c4d06fd39555af0
Iggyyy <ignacy.stepka@gmail.com>
2021-03-27 18:20:38 +0100

    Edit and save changes to events json

--- event.py
+++ event.py
@@ -2,7 +2,7 @@ import datetime, json, uuid, os
 """
 Types: TYPE_ZOOM, TYPE_BBB
 Date: datetime format
-Recurrent options: EVERY_WEEK, EVERY_OTHER_WEEK, DAILY, MONTHLY
+Recurring options: EVERY_WEEK, EVERY_OTHER_WEEK, DAILY, MONTHLY
 
 """
 JSON_PATH = "events.json"
@@ -13,44 +13,50 @@ class Event:
         '''Pass serialized json string'''
         self.s_json = json_data
         self.raw_dic = json.loads(json_data)
-        
-
 
     def save(self):
         '''Save event to main file'''
         m_json = {}
         if not os.path.isfile(JSON_PATH): #If file doesn't exist - create
             with open(JSON_PATH, "w") as outfile:
-                json.dump(m_json, outfile, indent=4)
+                json.dump(m_json, outfile, indent=4, ensure_ascii=False)
 
-        with open(JSON_PATH,) as outfile: #Read json and append 
+        with open(JSON_PATH, encoding='utf8') as outfile: #Read json and append 
             m_json = json.load(outfile)
         m_json[str(uuid.uuid4().hex)[0:20]] = self.raw_dic #append new json object
-        
-        with open(JSON_PATH, "w") as outfile: #Save file
-            json.dump(m_json, outfile, indent=4)
+        self.__save(m_json)
 
     def get_all_events(self):
         '''Return all events (serialized) saved in events.json'''        
-        with open(JSON_PATH,) as outfile:
+        with open(JSON_PATH, encoding='utf8') as outfile:
             return json.load(outfile)
 
+    def edit_json(self, event_to_change):
+        '''Update particular event (or events) in events.json'''
+        m_json = self.get_all_events()
+        
+        d_changes = event_to_change
+        for e_key in event_to_change.keys(): #Get key from event json
+            m_json[e_key] = d_changes[e_key] #Update all
+        
+        self.__save(m_json)
+        
+    def __save(self, dic):
+        with open(JSON_PATH, "w", encoding='utf8') as outfile: #Save file
+            json.dump(dic, outfile, indent=4, ensure_ascii=False)
 
-data ={}
-data['Title'] = "Angielski"
-data['Link'] = r"https://www.google.com"
-data['Comment'] = "Moje ulubione zajęcia"
-data['Date'] = str(datetime.datetime.now()) #TODO date
-data['Recurrent'] = "EVERY_WEEK" #TODO recurrent
-data['Type'] = "TYPE_ZOOM"
 
-#print(data)
 
-test_json = json.dumps(data, indent=4)
-#print(test_json)
-a = Event(test_json)
-a.save()
-print(a.get_all_events())
+# data ={}
+# data['Title'] = "Sysopsy"
+# data['Link'] = r"https://www.google.com"
+# data['Comment'] = "Moje ulubione zajęcia"
+# data['Date'] = str(datetime.datetime.now()) #TODO date
+# data['Recurring'] = "EVERY_WEEK" #TODO recurrent
+# data['Type'] = "TYPE_BBB"
 
-# date = datetime.datetime.now()
-# print(date.weekday())
+# s = { "d13b404a95544b6eb4f4" : data}
+# test_json = json.dumps(data, indent=4, ensure_ascii=False)
+# a = Event(test_json)
+# a.save()
+# a.edit_json(s)
