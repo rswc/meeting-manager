4f3803470a8ed79f8d16950134a561dd436dab86
rswc <rshoryti@protonmail.com>
2021-03-27 16:58:16 +0100

    Add native messaging test

--- extension/background.js
+++ extension/background.js
@@ -4,3 +4,20 @@ listener = browser.webRequest.onBeforeRequest.addListener(
     },
     {urls: ["*://*.mozilla.org/*"], types:["main_frame"]}
 );
+
+var port = browser.runtime.connectNative("meeting-manager");
+
+/*
+Listen for messages from the app.
+*/
+port.onMessage.addListener((response) => {
+  console.log("Received: " + response);
+});
+
+/*
+On a click on the browser action, send the app a message.
+*/
+browser.browserAction.onClicked.addListener(() => {
+  console.log("Sending:  ping");
+  port.postMessage("ping");
+});
