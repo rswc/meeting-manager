8bea731f48cf9c6af4864a990aaaa177845322f7
rswc <rshoryti@protonmail.com>
2021-03-28 03:25:04 +0200

    Add pretty recurring meetings

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,14 @@
 const ui = document.getElementsByTagName('*');
 var interval;
 
+REC_TRANS = {
+    "": "",
+    "EVERY_WEEK": "Weekly",
+    "EVERY_OTHER_WEEK": "Every other week",
+    "DAILY": "Daily",
+    "MONTHLY": "Monthly"
+}
+
 function timeDiff(timestamp, date) {
     let td = Math.floor(timestamp - Date.now() / 1000);
     if (Math.abs(td) <= 60) {
@@ -29,7 +37,7 @@ browser.runtime.onMessage.addListener((msg) => {
         for (const evt of msg.data) {
             let el = document.createElement("div");
             el.classList.add("evt");
-            el.innerHTML = `<div class="evt_title">${evt.Title}</div><div class="evt_time" data-date="${evt.Date}" data-timestamp="${evt.Timestamp}">00</div><div class="evt_misc">${evt.Recurring}</div>`;
+            el.innerHTML = `<div class="evt_title">${evt.Title}</div><div class="evt_time" data-date="${evt.Date}" data-timestamp="${evt.Timestamp}">00</div><div class="evt_misc">${REC_TRANS[evt.Recurring || '']}</div>`;
             ui.events.appendChild(el);
         }
         interval = window.setInterval(timeFormat, 500);
