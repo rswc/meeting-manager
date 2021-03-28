f6febf225c0f4523dfe219591d9d1182f0646363
rswc <rshoryti@protonmail.com>
2021-03-28 01:36:55 +0100

    Add up now section

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,39 @@
 const ui = document.getElementsByTagName('*');
+var interval;
+
+function timeDiff(timestamp, date) {
+    let td = Math.floor(timestamp - Date.now() / 1000);
+    if (Math.abs(td) <= 60) {
+        return `${td}s`;
+    }
+    td = Math.floor(td / 60);
+    if (Math.abs(td) <= 60) {
+        return `${td}min`;
+    }
+    return `${date}`;
+}
+
+function timeFormat() {
+    for (const el of ui.events.getElementsByClassName("evt_time")) {
+        el.innerText = timeDiff(el.dataset.timestamp, el.dataset.date);
+    }
+}
 
 document.getElementById('btn_add').addEventListener('click', () => {
     window.location.replace(browser.runtime.getURL("new_meeting_popup.html"));
+});
+
+browser.runtime.onMessage.addListener((msg) => {
+    if (msg.action === 'RECV' && Array.isArray(msg.data)) {
+        console.log(msg.data);
+        for (const evt of msg.data) {
+            let el = document.createElement("div");
+            el.classList.add("evt");
+            el.innerHTML = `<div class="evt_title">${evt.Title}</div><div class="evt_time" data-date="${evt.Date}" data-timestamp="${evt.Timestamp}">00</div><div class="evt_misc">${evt.Recurring}</div>`;
+            ui.events.appendChild(el);
+        }
+        interval = window.setInterval(timeFormat, 500);
+    }
 });
 
 browser.runtime.sendMessage({
