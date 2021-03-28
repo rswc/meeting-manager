768b4b4882f0eddaecd3c20ecab4a578875a7001
pavveu <pawel@ewnet.pl>
2021-03-28 01:06:03 +0100

    add notification in launch meeting

--- app/runner/launch_meeting.py
+++ app/runner/launch_meeting.py
@@ -1,14 +1,29 @@
 import webbrowser
+from win10toast import ToastNotifier 
+
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))[:-10]
+
+def notify(message: str, duration=5):
+    icon_path = PROJECT_PATH + r"app\runner\bingus.ico"
+    toast = ToastNotifier()
+    toast.show_toast("Meeting Manager", message, duration=duration, icon_path=icon_path)
 
 def launch_meeting(url, browser=None):
     webbrowser.get(browser).open_new_tab(url)
 
+ZOOM_URL = "https://zoom.us/j/6866658590?pwd=Y0d3c29OakpKQk1MT01ZbW5GVWpudz09"
+
 if __name__ == '__main__':
     # Add firefox to path
+    # th = threading.Thread(target=notify)
+
+    notify("Hello its Bingus")
 
     mozilla_path="C:\\Program Files\\Mozilla Firefox\\firefox.exe"
     webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(mozilla_path))
     url = "https://us02web.zoom.us/j/2732874617#success"
-    launch_meeting(url, browser='firefox')
 
+    launch_meeting(ZOOM_URL, browser='firefox')
     
