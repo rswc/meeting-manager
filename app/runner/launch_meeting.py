0b7cb5186bc28585f1622107f3b75bb81ebb588e
pavveu <pawel@ewnet.pl>
2021-03-28 01:39:56 +0100

    add launch meeting

--- app/runner/launch_meeting.py
+++ app/runner/launch_meeting.py
@@ -14,6 +14,7 @@ def launch_meeting(url, browser=None):
     webbrowser.get(browser).open_new_tab(url)
 
 ZOOM_URL = "https://zoom.us/j/6866658590?pwd=Y0d3c29OakpKQk1MT01ZbW5GVWpudz09"
+ZOOM_JOIN = "https://zoom.us/wc/6866658590/start" 
 
 if __name__ == '__main__':
     # Add firefox to path
@@ -25,5 +26,5 @@ if __name__ == '__main__':
     webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(mozilla_path))
     url = "https://us02web.zoom.us/j/2732874617#success"
 
-    launch_meeting(ZOOM_URL, browser='firefox')
+    launch_meeting(ZOOM_JOIN, browser='firefox')
     
